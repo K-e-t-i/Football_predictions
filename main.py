aa3c34ed3db3ed00cc8ccb12dff1054edc84b88a
keti98 <aniakettner@gmail,com>
2021-09-12 18:05:17 +0200

    change probs, add comments, show results

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def main():
     football_data = pd.read_csv("data.csv", header=0, usecols=[0, 1, 2, 3, 4, 5],
                                 names=["Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR"])
 
-    # read results 2 last team matches
+    # import results 2 last team matches
+    # Team_Matches = [list_of_home_team_scores, list_of_away_team_scores]
     Teams_Matches = [0, 0]
     for j in range(2):
         Matches = []
@@ -38,13 +39,13 @@ def main():
                 elif football_data["FTR"][i] == "H":
                     Matches.append(1)
 
-        # 2 last matches: Previous -> [0], Current [1]
+        # 2 last matches:  Previous -> [0], Current [1]
         Matches.reverse()
         del Matches[2:]
         Teams_Matches[j] = Matches
-    print(Teams_Matches)
+    # print(Teams_Matches)
 
-    # create a list of teams
+    # create a list of teams to make a ranking
     Teams = []
     team_list = football_data.copy()
     team_list.sort_values(["HomeTeam"], inplace=True)
@@ -79,22 +80,23 @@ def main():
     # Create ranking [('TeamName', Goals, Losses)] sorted by goals and losses
     Half_Sorted_Ranking = sorted(Ranking, key=itemgetter(4), reverse=False)
     Sorted_Ranking = sorted(Half_Sorted_Ranking, key=itemgetter(1), reverse=True)
-    print(Sorted_Ranking)
+    # print(Sorted_Ranking)
 
-    # ------------------------------------------------------------------
-    # create efficiency prob
+    # create extra efficiency based on scores
     home_efficiency = 0
     away_efficiency = 0
 
+    # add or sub efficiency depending on the position in the ranking
     for i in range(len(Sorted_Ranking)):
         if Sorted_Ranking[i][0] == Teams_to_predict[0]:
-            home_efficiency = 0.2 - i * 0.015
+            home_efficiency = 0.2 - i * 0.02
 
         elif Sorted_Ranking[i][0] == Teams_to_predict[1]:
-            away_efficiency = 0.2 - i * 0.015
+            away_efficiency = 0.2 - i * 0.02
 
     teams_eff = [home_efficiency, away_efficiency]
 
+    # ------------------------------------------------------------------------------
     # Create the model with edges specified as tuples (parent, child)
     edges = []
     for i in range(2):
@@ -123,19 +125,19 @@ def main():
 
         cpd_eff_prev.append(TabularCPD(variable='Eff_prev' + str(i),
                                        variable_card=2,
-                                       values=[[0.9, 0.1], [0.1, 0.9]],
+                                       values=[[0.85, 0.15], [0.15, 0.85]],
                                        evidence=['Eff_0' + str(i)],
                                        evidence_card=[2]))
 
         cpd_eff_curr.append(TabularCPD(variable='Eff_curr' + str(i),
                                        variable_card=2,
-                                       values=[[0.9, 0.1], [0.1, 0.9]],
+                                       values=[[0.85, 0.15], [0.15, 0.85]],
                                        evidence=['Eff_prev' + str(i)],
                                        evidence_card=[2]))
 
         cpd_eff_predict.append(TabularCPD(variable='Eff_predict' + str(i),
                                           variable_card=2,
-                                          values=[[0.9, 0.1], [0.1, 0.9]],
+                                          values=[[0.85, 0.15], [0.15, 0.85]],
                                           evidence=['Eff_curr' + str(i)],
                                           evidence_card=[2]))
 
@@ -161,30 +163,26 @@ def main():
     qH = football_infer.query(['Eff_predict0'],
                               evidence={'Result_prev0': Teams_Matches[0][0], 'Result_curr0': Teams_Matches[0][1]},
                               show_progress=False)
-    print('P(Eff_predict0|Results=\n', qH)
+    # print('P(Eff_predict0|Results=\n', qH)
     qA = football_infer.query(['Eff_predict1'],
                               evidence={'Result_prev1': Teams_Matches[1][0], 'Result_curr1': Teams_Matches[1][1]},
                               show_progress=False)
-    print('P(Eff_predict1|Results=\n', qA)
-
-    # qH = football_infer.query(['Eff_predict0'], evidence={'Result_prev0': 2, 'Result_curr0': 2}, show_progress=False)
-    # print('P(Eff_predict0|Results=\n', qH)
-    # qA = football_infer.query(['Eff_predict1'], evidence={'Result_prev1': 0, 'Result_curr1': 1}, show_progress=False)
     # print('P(Eff_predict1|Results=\n', qA)
+    # ------------------------------------------------------------------------------
 
+    # Sum up the efficiency of teams
     good_eff_H = qH.values[0]
-    good_eff_H += teams_eff[0]
-
     good_eff_A = qA.values[0]
+
+    # add or sub efficiency depending on the position in the ranking
+    good_eff_H += teams_eff[0]
     good_eff_A += teams_eff[1]
 
+    # Subtracting efficiency
     sub = abs(good_eff_A - good_eff_H)
 
-    print('Sub: ', sub)
-    print('Home: ', good_eff_H)
-    print('Away: ', good_eff_A)
-
-    if sub > 0.1:
+    # Show result depending on the efficiency
+    if sub > 0.08:
         if good_eff_H > good_eff_A:
             print('H')
         else:
